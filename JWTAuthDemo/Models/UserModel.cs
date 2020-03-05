bcb8de58914a0e1f1eded0ec2bb7ee18a2057da1
Famous Fortune <38983082+Mystery15@users.noreply.github.com>
2020-03-05 15:19:21 +0100

    First Commit

---
+++ JWTAuthDemo/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuthDemo.Models
+{
+    public class UserModel
+    {
+        public string Username { set; get; }
+        public string Password { set; get; }
+        public string EmailAddress { set; get; }
+    }
+}
